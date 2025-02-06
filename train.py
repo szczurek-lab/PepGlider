5ed45173257549a9736277b03837fde9c518db59
agwiazda <01161989@pw.edu.pl>
2025-02-06 21:58:04 +0100

    better performance of wae

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import os
-from torch import optim, nn, utils, load, device, cuda, save, zeros, float32, transpose, LongTensor, zeros_like, ones_like
+from torch import optim, nn, utils, logsumexp, device, cuda, save, zeros, float32, transpose, LongTensor, zeros_like, ones_like
 from torch.distributions import Normal
 from model.model import EncoderRNN, DecoderRNN#, VAE
 # import lightning as L
@@ -19,9 +19,9 @@ import itertools
 from pathlib import Path
 from tqdm import tqdm
 import data.dataset as dataset
-from model.constants import MIN_LENGTH, MAX_LENGTH
+from model.constants import MIN_LENGTH, MAX_LENGTH, VOCAB_SIZE
 
-ROOT_DIR = Path(__file__).parent.parent
+ROOT_DIR = Path(__file__).parent#.parent
 DATA_DIR = ROOT_DIR / "data"
 MODELS_DIR = ROOT_DIR
 from typing import Optional, Literal
@@ -77,7 +77,7 @@ amp_x, amp_y = data_manager.get_merged_data()
 # x_tensor = tensor(tab)
 # y_tensor = tensor(y)
 dataset = utils.data.TensorDataset(amp_x, tensor(amp_y, dtype=long))
-train_loader = utils.data.DataLoader(amp_x, batch_size=512)
+train_loader = utils.data.DataLoader(amp_x, batch_size=512, shuffle=True)
 # e = EncoderRNN(25, 512, 100, 2, bidirectional=True).to(DEVICE)
 # d = DecoderRNN(100, 512, 21, 2).to(DEVICE)
 # autoencoder = VAE(e, d)
@@ -93,17 +93,18 @@ params = {
     "latent_dim": 64,
     "encoding": "add",
     "dropout": 0.1,
-    "batch_size": 256,
+    "batch_size": 512,
     "lr": 0.001,
-    "kl_beta": 0.1,
+    "kl_beta_schedule": (0.1, 1.0, 2000),
     "train_size": None,
     "epochs": 10000,
+    "iwae_samples": 16,
     "model_name": "basic",
     "use_clearml": True,
-    # "use_clearml": False,
-    "task_name": "vae",
+    "task_name": "iwae_progressive_beta_v7",
     "device": "cuda",
     "deeper_eval_every": 20,
+    "save_model_every": 100,
 }
 encoder = EncoderRNN(
     params["num_heads"],
@@ -124,16 +125,22 @@ decoder = DecoderRNN(
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 def report_scalars(
     logger: clearml.Logger,
-    series: str,
+    hue: str,
     epoch: int,
-    scalars: List[Tuple[str, float]],
+    scalars: list[tuple[str, float] | tuple[str, str, float]],
 ):
-    for name, val in scalars:
-        logger.report_scalar(title=name, series=series, value=val, iteration=epoch)
+    for tpl in scalars:
+        if len(tpl) == 2:
+            name, val = tpl
+            current_hue = hue
+        else:
+            name, hue_suffix, val = tpl
+            current_hue = f"{hue} - {hue_suffix}"
+        logger.report_scalar(title=name, series=current_hue, value=val, iteration=epoch)
 
 def report_sequence_char(
     logger: clearml.Logger,
-    series: str,
+    hue: str,
     epoch: int,
     seq_true: np.ndarray,
     model_out: np.ndarray,
@@ -173,27 +180,27 @@ def report_sequence_char(
 
     logger.report_scalar(
         title="Length Prediction Accuracy",
-        series=series,
+        series=hue,
         value=pred_len_acc,
         iteration=epoch,
     )
     logger.report_scalar(
-        title="Length Loss [mae]", series=series, value=pred_len_mae, iteration=epoch
+        title="Length Loss [mae]", series=hue, value=pred_len_mae, iteration=epoch
     )
     logger.report_scalar(
         title="Token Prediction Accuracy (on predicted length)",
-        series=series,
+        series=hue,
         value=on_predicted_acc,
         iteration=epoch,
     )
     logger.report_scalar(
-        title="Amino Token Accuracy", series=series, value=amino_acc, iteration=epoch
+        title="Amino Token Accuracy", series=hue, value=amino_acc, iteration=epoch
     )
     logger.report_scalar(
-        title="Empty Token Accuracy", series=series, value=empty_acc, iteration=epoch
+        title="Empty Token Accuracy", series=hue, value=empty_acc, iteration=epoch
     )
 
-def run_epoch_vae(
+def run_epoch_iwae(
     mode: Literal["test", "train"],
     encoder: EncoderRNN,
     decoder: DecoderRNN,
@@ -204,21 +211,35 @@ def run_epoch_vae(
     logger: Optional[clearml.Logger],
     optimizer: Optional[optim.Optimizer],
     eval_mode: Literal["fast", "deep"],
+    iwae_samples: int,
 ):
-    ce_loss_fun = nn.CrossEntropyLoss(reduction="sum")
+    ce_loss_fun = nn.CrossEntropyLoss(reduction="none")
     encoder.to(device), decoder.to(device)
     if mode == "train":
         encoder.train(), decoder.train()
     else:
         encoder.eval(), decoder.eval()
 
-    stat_sum = {"kl": 0.0, "ce": 0.0, "std": 0.0, "total": 0.0}
-    seq_true, model_out = [], []
+    stat_sum = {
+        "kl_mean": 0.0,
+        "kl_best": 0.0,
+        "kl_worst": 0.0,
+        "ce_mean": 0.0,
+        "ce_best": 0.0,
+        "ce_worst": 0.0,
+        "std": 0.0,
+        "total": 0.0,
+    }
+    seq_true, model_out, model_out_sampled = [], [], []
     len_data = len(dataloader.dataset)
+
+    K = iwae_samples
+    C = VOCAB_SIZE + 1
 
     for batch in dataloader:
         # S x B
         peptides = batch.permute(1, 0).type(LongTensor).to(device)
+        S, B = peptides.shape
         if optimizer:
             optimizer.zero_grad()
 
@@ -227,58 +248,90 @@ def run_epoch_vae(
 
         prior_distr = Normal(zeros_like(mu), ones_like(std))
         q_distr = Normal(mu, std)
-        z = q_distr.rsample()
+        z = q_distr.rsample((K,))
 
         # Kullback Leibler divergence
-        log_qzx = q_distr.log_prob(z).sum(dim=1)
-        log_pz = prior_distr.log_prob(z).sum(dim=1)
+        log_qzx = q_distr.log_prob(z).sum(dim=2)
+        log_pz = prior_distr.log_prob(z).sum(dim=2)
 
-        kl_div = (log_qzx - log_pz).sum()
+        kl_div = log_qzx - log_pz
 
         # reconstruction - cross entropy
-        sampled_peptide_logits = decoder(z)  # S x B x C
-        S, B, C = sampled_peptide_logits.shape
-        input_reshaped = sampled_peptide_logits.permute(1, 0, 2).reshape(B * S, C)
-        target_reshaped = peptides.permute(1, 0).reshape(B * S)
+        sampled_peptide_logits = decoder(z.reshape(K * B, -1)).reshape(S, K, B, C)
+        src = sampled_peptide_logits.permute(1, 3, 2, 0)  # K x C x B x S
+        tgt = peptides.permute(1, 0).reshape(1, B, S).repeat(K, 1, 1)  # K x B x S
 
+        # K x B
         cross_entropy = ce_loss_fun(
-            input_reshaped,
-            target_reshaped,
-        )
+            src,
+            tgt,
+        ).sum(dim=2)
 
-        loss = (cross_entropy + kl_beta * kl_div) / len(batch)
+        loss = logsumexp(
+            cross_entropy + kl_beta * (log_qzx - log_pz), dim=0
+        ).mean(dim=0)
 
-        stat_sum["ce"] += cross_entropy.item()
-        stat_sum["kl"] += kl_div.item()
+        # stats
+        stat_sum["kl_mean"] += kl_div.mean(dim=0).sum(dim=0).item()
+        stat_sum["kl_best"] += kl_div.min(dim=0).values.sum(dim=0).item()
+        stat_sum["kl_worst"] += kl_div.max(dim=0).values.sum(dim=0).item()
+        stat_sum["ce_mean"] += cross_entropy.mean(dim=0).sum(dim=0).item()
+        stat_sum["ce_best"] += cross_entropy.min(dim=0).values.sum(dim=0).item()
+        stat_sum["ce_worst"] += cross_entropy.max(dim=0).values.sum(dim=0).item()
         stat_sum["total"] += loss.item() * len(batch)
-        stat_sum["std"] += std.mean(axis=1).sum().item()
+        stat_sum["std"] += std.mean(dim=1).sum().item()
 
         if optimizer:
             loss.backward()
             optimizer.step()
 
         # reporting
-        seq_true.append(peptides.cpu().detach().numpy())
-        model_out.append(sampled_peptide_logits.cpu().detach().numpy())
+        if eval_mode == "deep":
+            seq_true.append(peptides.cpu().detach().numpy())
+            model_out.append(decoder(mu).cpu().detach().numpy())
+            model_out_sampled.append(
+                sampled_peptide_logits[:, 0, :, :].cpu().detach().numpy()
+            )
+
     if logger is not None:
         report_scalars(
             logger,
             mode,
             epoch,
             scalars=[
-                ("Cross Entropy Loss", stat_sum["ce"] / len_data),
-                ("KL Divergence", stat_sum["kl"] / len_data),
                 ("Total Loss", stat_sum["total"] / len_data),
                 ("Posterior Standard Deviation [mean]", stat_sum["std"] / len_data),
+                (
+                    "Cross Entropy Loss",
+                    "mean over samples",
+                    stat_sum["ce_mean"] / len_data,
+                ),
+                ("Cross Entropy Loss", "best sample", stat_sum["ce_best"] / len_data),
+                ("Cross Entropy Loss", "worst sample", stat_sum["ce_worst"] / len_data),
+                (
+                    "KL Divergence",
+                    "mean over samples",
+                    stat_sum["kl_mean"] / len_data,
+                ),
+                ("KL Divergence", "best sample", stat_sum["kl_best"] / len_data),
+                ("KL Divergence", "worst sample", stat_sum["kl_worst"] / len_data),
+                ("KL Beta", kl_beta),
             ],
         )
         if eval_mode == "deep":
             report_sequence_char(
                 logger,
-                mode,
-                epoch,
+                hue=f"{mode} - mu",
+                epoch=epoch,
                 seq_true=np.concatenate(seq_true, axis=1),
                 model_out=np.concatenate(model_out, axis=1),
+            )
+            report_sequence_char(
+                logger,
+                hue=f"{mode} - z",
+                epoch=epoch,
+                seq_true=np.concatenate(seq_true, axis=1),
+                model_out=np.concatenate(model_out_sampled, axis=1),
             )
     return stat_sum["total"] / len_data
 
@@ -304,7 +357,9 @@ def run():
     best_loss = 1e18
     for epoch in tqdm(range(params["epochs"])):
         eval_mode = "deep" if epoch % params["deeper_eval_every"] == 0 else "fast"
-        run_epoch_vae(
+        beta_0, beta_1, t_1 = params["kl_beta_schedule"]
+        kl_beta = min(beta_0 + (beta_1 - beta_0) / t_1 * epoch, 1.0)
+        run_epoch_iwae(
             mode="train",
             encoder=encoder,
             decoder=decoder,
@@ -313,11 +368,12 @@ def run():
             logger=logger,
             epoch=epoch,
             optimizer=optimizer,
-            kl_beta=params["kl_beta"],
+            kl_beta=kl_beta,
             eval_mode=eval_mode,
+            iwae_samples=params["iwae_samples"],
         )
         if eval_mode == "deep":
-            loss = run_epoch_vae(
+            loss = run_epoch_iwae(
                 mode="test",
                 encoder=encoder,
                 decoder=decoder,
@@ -326,16 +382,32 @@ def run():
                 logger=logger,
                 epoch=epoch,
                 optimizer=None,
-                kl_beta=params["kl_beta"],
+                kl_beta=kl_beta,
                 eval_mode=eval_mode,
+                iwae_samples=params["iwae_samples"],
             )
-            if loss < best_loss:
-                best_loss = loss
+            # if loss < best_loss and False:
+            #     best_loss = loss
+            #     save_model(
+            #         encoder,
+            #         f"{params['task_name']}_{params['model_name']}_best_encoder.pt",
+            #         with_hash=False,
+            #     )
+            #     save_model(
+            #         decoder,
+            #         f"{params['task_name']}_{params['model_name']}_best_decoder.pt",
+            #         with_hash=False,
+            #     )
+            if epoch > 0 and epoch % params["save_model_every"] == 0:
                 save_model(
-                    encoder, f"{params['task_name']}_{params['model_name']}_encoder.pt"
+                    encoder,
+                    f"{params['task_name']}_{params['model_name']}_epoch{epoch}_encoder.pt",
+                    with_hash=False,
                 )
                 save_model(
-                    decoder, f"{params['task_name']}_{params['model_name']}_decoder.pt"
+                    decoder,
+                    f"{params['task_name']}_{params['model_name']}_epoch{epoch}_decoder.pt",
+                    with_hash=False,
                 )
 run()
 # autoencoder.load_state_dict(load('./gmm_model.pt'))

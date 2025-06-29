1a466101f346a7a539d398653dc1b39bdb12c028
Aleksandra Gwiazda <gwiazale@rudy.mimuw.edu.pl>
2025-06-29 17:45:45 +0200

    from rudy

--- train.py
+++ train.py
@@ -337,7 +337,7 @@ def run():#rank, world_size
         "device": "cuda",
         "deeper_eval_every": 20,
         "save_model_every": 100,
-        "reg_dim": [0], # [hydrophobicity_moment, length, charge]
+        "reg_dim": [1], # [length, charge, hydrophobicity_moment]
         "gamma_schedule": (1, 200, 8000)
     }
     encoder = EncoderRNN(

d0acaab84d5347795ef59c2e9a345087e8cec5a2
agwiazda <01161989@pw.edu.pl>
2025-07-02 16:23:54 +0200

    again

--- train_slurm.py
+++ train_slurm.py
@@ -227,7 +227,7 @@ def run_epoch_iwae(
         iwae_terms_stacked = torch.stack(iwae_terms, dim=0).reshape(-1)
 
         loss = logsumexp(iwae_terms_stacked, dim=0)
-        total_reg_loss = torch.stack(reg_losses_per_sample_list, dim=0).reshape(-1)
+        total_reg_loss = torch.stack(reg_losses_per_sample_list, dim=0).reshape(-1).sum()
         loss += total_reg_loss
         print(f'loss = {loss}')
         stacked_kl_divs = torch.stack(all_kl_divs, dim=0).reshape(-1)  

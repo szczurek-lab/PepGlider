cb4ef3dd184d650a5ff92fa69f57919b19e06edf
agwiazda <01161989@pw.edu.pl>
2025-08-02 23:22:28 +0200

    again

--- monitoring.py
+++ monitoring.py
@@ -248,6 +248,9 @@ def report_sequence_char_test(
             metrics_list = [pred_len_acc, pred_len_mae, on_predicted_acc, amino_acc, empty_acc, mean_absolute_error(physchem_original[indexes,1], physchem_decoded.iloc[:,1]), mean_absolute_error(physchem_original[indexes,2], physchem_decoded.iloc[:,2]), mean_absolute_error(physchem_original[indexes,0], physchem_decoded.iloc[:,0])]
         for attr in metrics.keys():
                 for subattr in metrics[attr].keys():
+                    if attr == 'Interpretability':
                         # print(metrics[attr][subattr])
                         metrics_list = metrics_list + [metrics[attr][subattr][1]]
+                    else:
+                        metrics_list = metrics_list + [metrics[attr][subattr]]
     return metrics_list

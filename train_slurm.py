eec35e8bbd7efbeb0289d14ae240f4b43e1d8218
agwiazda <01161989@pw.edu.pl>
2025-10-06 16:14:59 +0200

    corrected dataset

--- train_slurm.py
+++ train_slurm.py
@@ -4,9 +4,9 @@ from training_functions import set_seed, run
 if __name__ == '__main__':
     set_seed()
     # print('AMPs/nonAMPs')
-    run(['positiv_negativ_AMPs'])
+    # run(['positiv_negativ_AMPs'])
     # run(['positiv_AMPs'], encoder_filepath, decoder_filepath)
-    # run(['positiv_AMPs'])
+    run(['positiv_AMPs'])
     # run(['positiv_negativ_AMPs'], encoder_filepath, decoder_filepath)
     #run(['uniprot'], encoder_filepath, decoder_filepath)
     # print('merged data AMPs/nonAMPs + Uniprot')

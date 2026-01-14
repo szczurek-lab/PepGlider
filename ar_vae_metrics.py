3d008a43c73a249fc536ccca310c98128ae22195
agwiazda <01161989@pw.edu.pl>
2026-01-14 20:15:43 +0100

    error corrected

--- ar_vae_metrics.py
+++ ar_vae_metrics.py
@@ -46,6 +46,7 @@ def compute_interpretability_metric(latent_codes_input, attributes, attr_list):
     latent_codes = latent_codes_input
     for i, attr_name in tqdm(enumerate(attr_list)):
         if attr_name == 'MIC E.coli' or attr_name == 'MIC S.aureus':
+            latent_codes = latent_codes_input
             finite_mask = np.isfinite(attributes[:,i])
             latent_codes = latent_codes_input[finite_mask,:]
             attr_labels = attributes[finite_mask, i]

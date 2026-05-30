07ac3b32b6b1b5e1efa6ab2de228a4d62edb925e
agwiazda <01161989@pw.edu.pl>
2026-05-30 22:05:35 +0200

    params setting cleaned

--- params_setting.py
+++ params_setting.py
@@ -2,152 +2,88 @@ import clearml
 import os
 import datetime
 import csv
+import argparse
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 
 def set_params(root_dir):
-    params = {
-        "num_heads": 4,
-        "num_layers": 6,
-        "layer_norm": True,
-        "latent_dim": 56,
-        "encoding": "add",
-        "dropout": 0.1,
-        "batch_size": 512,
-        "lr": 0.001,
-        "kl_beta_schedule": (0.00001, 0.1, 8000),
-        "train_size": None,
-        "epochs": 8000,
-        "iwae_samples": 10,
-        "model_name": os.getenv("CLEARML_PROJECT_NAME", 'ar-vae-v4'),
-        # "use_clearml": False,
-        "task_name": os.getenv("CLEARML_TASK_NAME", "ar-vae 3 dims"),
-        "device": "cuda",
-        "deeper_eval_every": 20,
-        "save_model_every": 100,
-        "ar_vae_flg": True,
-        "reg_dim": [3,4,5], 
-        "gamma_schedule": (0.00001, 20, 8000),
-        "gamma_multiplier": [1,1,1,1,1,1],
-        "factor_multiplier": [0.1,0.1,0.1,0.6,0.6,0.6],
-        "factor_schedule": (1,1,8000),
-        'scale_factor_flg': True,
-        'mic_flg': True,
-        'toxicity_flg': True,
-        'normalize_properties_flg':True,
-        'signum_modification_of_dist_matrix_flg': True
-    }
+    parser = argparse.ArgumentParser(description="AR-VAE training parameters")
 
-    # if params["use_clearml"]:
-    #     task = clearml.Task.init(
-    #         project_name=os.getenv("CLEARML_PROJECT_NAME", 'ar-vae-v4'), task_name=os.getenv("CLEARML_TASK_NAME", "ar-vae 3 dims")
-    #     )
-    #     task.set_parameters(params)
-    #     logger = task.logger
-    #     train_log_file = None
-    #     eval_log_file = None
-    # else:
-    # logger = None
+    parser.add_argument("--num_heads", type=int, default=4)
+    parser.add_argument("--num_layers", type=int, default=6)
+    parser.add_argument("--layer_norm", type=str2bool, default=True)
+    parser.add_argument("--latent_dim", type=int, default=56)
+    parser.add_argument("--encoding", type=str, default="add")
+    parser.add_argument("--dropout", type=float, default=0.1)
+    parser.add_argument("--batch_size", type=int, default=512)
+    parser.add_argument("--lr", type=float, default=0.001)
+    parser.add_argument("--kl_beta_schedule", type=float, nargs=3, default=[0.00001, 0.1, 8000])
+    parser.add_argument("--train_size", type=int, default=None)
+    parser.add_argument("--epochs", type=int, default=8000)
+    parser.add_argument("--iwae_samples", type=int, default=10)
+    parser.add_argument("--model_name", type=str, default=os.getenv("CLEARML_PROJECT_NAME", 'ar-vae-v4'))
+    parser.add_argument("--task_name", type=str, default=os.getenv("CLEARML_TASK_NAME", "ar-vae 3 dims"))
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--deeper_eval_every", type=int, default=20)
+    parser.add_argument("--save_model_every", type=int, default=100)
+    parser.add_argument("--ar_vae_flg", type=str2bool, default=True)
+    parser.add_argument("--reg_dim", type=int, nargs='+', default=[3, 4, 5])
+    parser.add_argument("--gamma_schedule", type=float, nargs=3, default=[0.00001, 20, 8000])
+    parser.add_argument("--gamma_multiplier", type=float, nargs='+', default=[1, 1, 1, 1, 1, 1])
+    parser.add_argument("--factor_multiplier", type=float, nargs='+', default=[0.1, 0.1, 0.1, 0.6, 0.6, 0.6])
+    parser.add_argument("--factor_schedule", type=float, nargs=3, default=[1, 1, 8000])
+    parser.add_argument("--scale_factor_flg", type=str2bool, default=True)
+    parser.add_argument("--mic_flg", type=str2bool, default=True)
+    parser.add_argument("--toxicity_flg", type=str2bool, default=True)
+    parser.add_argument("--normalize_properties_flg", type=str2bool, default=True)
+    parser.add_argument("--signum_modification_of_dist_matrix_flg", type=str2bool, default=True)
+
+    args, _ = parser.parse_known_args()
+
+    params = vars(args)
+    params["kl_beta_schedule"] = tuple(params["kl_beta_schedule"])
+    params["gamma_schedule"] = tuple(params["gamma_schedule"])
+    params["factor_schedule"] = tuple(params["factor_schedule"])
+
+    # Build attribute names based on reg_dim (matching ar_vae_metrics.extract_relevant_attributes)
+    all_attr_names = {0: 'length', 1: 'charge', 2: 'hydrophobicity', 3: 'mic_e_coli', 4: 'mic_s_aureus', 5: 'nontoxicity'}
+    attr_names = [all_attr_names[i] for i in params["reg_dim"]]
+
+    # Build training log header
     train_log_file = f'training_log_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv'.replace(' ', '_')
     with open(root_dir / train_log_file, 'a', newline='') as csvfile:
+        header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss", "KL Div", "KL Div * Beta"]
         if params["ar_vae_flg"]:
-            header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss","KL Div","KL Div * Beta","Reg Loss", "Reg Loss * Gamma", "Delta",] 
+            header += ["Reg Loss", "Reg Loss * Gamma", "Delta"]
             if params["scale_factor_flg"]:
-                    header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss","KL Div","KL Div * Beta","Reg Loss", "Reg Loss * Gamma", "Delta","Scale factor",]
-        else:
-            header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss", "KL Div", "KL Div * Beta"]
+                header += ["Scale factor"]
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(header)
+
+    # Build eval log header
     eval_log_file = f'validation_log_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv'.replace(' ', '_')
     with open(root_dir / eval_log_file, 'a', newline='') as csvfile:
+        header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss", "KL Div", "KL Div * Beta"]
         if params["ar_vae_flg"]:
-            header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss","KL Div","KL Div * Beta","Reg Loss", "Reg Loss * Gamma", "Delta", 
-                      "Length Pred Acc", "Length Loss [mae]", "Token Pre Acc", "Amino Acc", "Empty Acc", 
-                      "MAE length", "MAE charge", "MAE hydrophobicity moment", 
-                      "Interpretability - length", "Interpretability - charge", "Interpretability - hydrophobicity","Interpretability - mean",
-                      "Corr_score - length", "Corr_score - charge", "Corr_score - hydrophobicity","Corr_score - mean",
-                      "Modularity - length", "Modularity - charge", "Modularity - hydrophobicity","Modularity - mean",
-                      "MIG - length", "MIG - charge", "MIG - hydrophobicity moment","MIG - mean",
-                      "SAP_score - length", "SAP_score - charge", "SAP_score - hydrophobicity","SAP_score - mean"
-                      ] 
+            header += ["Reg Loss", "Reg Loss * Gamma", "Delta"]
             if params["scale_factor_flg"]:
-                header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss","KL Div","KL Div * Beta","Reg Loss", "Reg Loss * Gamma", "Delta", "Scale factor",
-                        "Length Pred Acc", "Length Loss [mae]", "Token Pre Acc", "Amino Acc", "Empty Acc", 
-                        "MAE length", "MAE charge", "MAE hydrophobicity moment", 
-                        "Interpretability - length", "Interpretability - charge", "Interpretability - hydrophobicity","Interpretability - mean",
-                        "Corr_score - length", "Corr_score - charge", "Corr_score - hydrophobicity","Corr_score - mean",
-                        "Modularity - length", "Modularity - charge", "Modularity - hydrophobicity","Modularity - mean",
-                        "MIG - length", "MIG - charge", "MIG - hydrophobicity moment","MIG - mean",
-                        "SAP_score - length", "SAP_score - charge", "SAP_score - hydrophobicity","SAP_score - mean"
-                    ] 
-            if params['mic_flg'] and params['toxicity_flg'] and (1 not in params['reg_dim'] or 2 not in params['reg_dim'] or 3 not in params['reg_dim']):
-                header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss","KL Div","KL Div * Beta","Reg Loss", "Reg Loss * Gamma", "Delta", 
-                        "Length Pred Acc", "Length Loss [mae]", "Token Pre Acc", "Amino Acc", "Empty Acc", 
-                        "MAE length", "MAE charge", "MAE hydrophobicity moment", 
-                        "Interpretability - mic_e_cola", "Interpretability - mic_s_aureus", "Interpretability - nontoxicity", "Interpretability - mean",
-                        "Corr_score - mic_e_cola", "Corr_score - mic_s_aureus", "Corr_score - nontoxicity","Corr_score - mean",
-                        "Modularity - mic_e_cola", "Modularity - mic_s_aureus", "Modularity - nontoxicity","Modularity - mean",
-                        "MIG - mic_e_cola", "MIG - mic_s_aureus", "MIG - nontoxicity","MIG - mean",
-                        "SAP_score - mic_e_cola", "SAP_score - mic_s_aureus", "SAP_score - nontoxicity","SAP_score - mean"
-                        ] 
-            if params['mic_flg'] and (1 not in params['reg_dim'] or 2 not in params['reg_dim'] or 3 not in params['reg_dim']):
-                header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss","KL Div","KL Div * Beta","Reg Loss", "Reg Loss * Gamma", "Delta", 
-                        "Length Pred Acc", "Length Loss [mae]", "Token Pre Acc", "Amino Acc", "Empty Acc", 
-                        "MAE length", "MAE charge", "MAE hydrophobicity moment", 
-                        "Interpretability - mic_e_cola", "Interpretability - mic_s_aureus","Interpretability - mean",
-                        "Corr_score - mic_e_cola", "Corr_score - mic_s_aureus","Corr_score - mean",
-                        "Modularity - mic_e_cola", "Modularity - mic_s_aureus","Modularity - mean",
-                        "MIG - mic_e_cola", "MIG - mic_s_aureus","MIG - mean",
-                        "SAP_score - mic_e_cola", "SAP_score - mic_s_aureus","SAP_score - mean"
-                        ] 
-            if params['mic_flg']:
-                header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss","KL Div","KL Div * Beta","Reg Loss", "Reg Loss * Gamma", "Delta", 
-                        "Length Pred Acc", "Length Loss [mae]", "Token Pre Acc", "Amino Acc", "Empty Acc", 
-                        "MAE length", "MAE charge", "MAE hydrophobicity moment", 
-                        "Interpretability - length", "Interpretability - charge", "Interpretability - hydrophobicity", "Interpretability - mic_e_cola", "Interpretability - mic_s_aureus","Interpretability - mean",
-                        "Corr_score - length", "Corr_score - charge", "Corr_score - hydrophobicity", "Corr_score - mic_e_cola", "Corr_score - mic_s_aureus","Corr_score - mean",
-                        "Modularity - length", "Modularity - charge", "Modularity - hydrophobicity", "Modularity - mic_e_cola", "Modularity - mic_s_aureus","Modularity - mean",
-                        "MIG - length", "MIG - charge", "MIG - hydrophobicity moment", "MIG - mic_e_cola", "MIG - mic_s_aureus","MIG - mean",
-                        "SAP_score - length", "SAP_score - charge", "SAP_score - hydrophobicity", "SAP_score - mic_e_cola", "SAP_score - mic_s_aureus","SAP_score - mean"
-                        ] 
-        else:
-            header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss","KL Div","KL Div * Beta",
-                      "Length Pred Acc", "Length Loss [mae]", "Token Pre Acc", "Amino Acc", "Empty Acc", 
-                      "MAE length", "MAE charge", "MAE hydrophobicity moment", 
-                      "Interpretability - length", "Interpretability - charge", "Interpretability - hydrophobicity","Interpretability - mean",
-                      "Corr_score - length", "Corr_score - charge", "Corr_score - hydrophobicity","Corr_score - mean",
-                      "Modularity - length", "Modularity - charge", "Modularity - hydrophobicity","Modularity - mean",
-                      "MIG - length", "MIG - charge", "MIG - hydrophobicity moment","MIG - mean",
-                      "SAP_score - length", "SAP_score - charge", "SAP_score - hydrophobicity","SAP_score - mean"
-                      ] 
-            if params['mic_flg'] and params['toxicity_flg'] and (1 not in params['reg_dim'] or 2 not in params['reg_dim'] or 3 not in params['reg_dim']):
-                header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss","KL Div","KL Div * Beta",
-                        "Length Pred Acc", "Length Loss [mae]", "Token Pre Acc", "Amino Acc", "Empty Acc", 
-                        "MAE length", "MAE charge", "MAE hydrophobicity moment", 
-                        "Interpretability - mic_e_cola", "Interpretability - mic_s_aureus", "Interpretability - nontoxicity", "Interpretability - mean",
-                        "Corr_score - mic_e_cola", "Corr_score - mic_s_aureus", "Corr_score - nontoxicity","Corr_score - mean",
-                        "Modularity - mic_e_cola", "Modularity - mic_s_aureus", "Modularity - nontoxicity","Modularity - mean",
-                        "MIG - mic_e_cola", "MIG - mic_s_aureus", "MIG - nontoxicity","MIG - mean",
-                        "SAP_score - mic_e_cola", "SAP_score - mic_s_aureus", "SAP_score - nontoxicity","SAP_score - mean"
-                        ] 
-            if params['mic_flg'] and (1 not in params['reg_dim'] or 2 not in params['reg_dim'] or 3 not in params['reg_dim']):
-                header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss","KL Div","KL Div * Beta", 
-                        "Length Pred Acc", "Length Loss [mae]", "Token Pre Acc", "Amino Acc", "Empty Acc", 
-                        "MAE length", "MAE charge", "MAE hydrophobicity moment", 
-                        "Interpretability - mic_e_cola", "Interpretability - mic_s_aureus","Interpretability - mean",
-                        "Corr_score - mic_e_cola", "Corr_score - mic_s_aureus","Corr_score - mean",
-                        "Modularity - mic_e_cola", "Modularity - mic_s_aureus","Modularity - mean",
-                        "MIG - mic_e_cola", "MIG - mic_s_aureus","MIG - mean",
-                        "SAP_score - mic_e_cola", "SAP_score - mic_s_aureus","SAP_score - mean"
-                        ] 
-            if params['mic_flg']:
-                header = ["Mode", "Epoch", "Total Loss", "Cross Entropy Loss","KL Div","KL Div * Beta",
-                        "Length Pred Acc", "Length Loss [mae]", "Token Pre Acc", "Amino Acc", "Empty Acc", 
-                        "MAE length", "MAE charge", "MAE hydrophobicity moment", 
-                        "Interpretability - length", "Interpretability - charge", "Interpretability - hydrophobicity", "Interpretability - mic_e_cola", "Interpretability - mic_s_aureus","Interpretability - mean",
-                        "Corr_score - length", "Corr_score - charge", "Corr_score - hydrophobicity", "Corr_score - mic_e_cola", "Corr_score - mic_s_aureus","Corr_score - mean",
-                        "Modularity - length", "Modularity - charge", "Modularity - hydrophobicity", "Modularity - mic_e_cola", "Modularity - mic_s_aureus","Modularity - mean",
-                        "MIG - length", "MIG - charge", "MIG - hydrophobicity moment", "MIG - mic_e_cola", "MIG - mic_s_aureus","MIG - mean",
-                        "SAP_score - length", "SAP_score - charge", "SAP_score - hydrophobicity", "SAP_score - mic_e_cola", "SAP_score - mic_s_aureus","SAP_score - mean"
-                        ] 
+                header += ["Scale factor"]
+        header += ["Length Pred Acc", "Length Loss [mae]", "Token Pre Acc", "Amino Acc", "Empty Acc"]
+        header += ["MAE length", "MAE charge", "MAE hydrophobicity moment"]
+        for metric in ["Interpretability", "Corr_score", "Modularity", "MIG", "SAP_score"]:
+            for attr in attr_names + ["mean"]:
+                header.append(f"{metric} - {attr}")
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(header)
-    return params, train_log_file, eval_log_file #, logger
+
+    return params, train_log_file, eval_log_file
